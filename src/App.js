2bf1e842fcfe5c3cb2c11a66b217feb7c69a3dc4
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-07-02 22:48:26 -0700

    created skeleton for the anime component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import Landing from './Landing';
+import Landing from './components/Landing';
 
 function App() {
   return (
---
+++ src/components/Anime.js
@@ -0,0 +1,17 @@
+import React from "react";
+export default function Anime(){
+    // add state for the page number and to save data from api call
+    // do the api call to get the info from the backend use axios or a regular fetch (up to future me (: )
+    // try catch for error handling
+    // funtion to increment or decrement the page
+    
+
+
+
+
+
+    return(
+        <>
+        </>
+    );
+}
