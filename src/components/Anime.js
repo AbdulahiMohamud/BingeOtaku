cd813abd6e3be3e3f6199d6557fcef9b6267481d
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-07-18 15:47:53 -0700

    removed the mouse hover modal and replaced it with a click instead due to glitching

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -17,17 +17,13 @@ export default function Anime() {
   const [selectedGenres, setSelectedGenres] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [selectedAnime, setSelectedAnime] = useState(null);
-  let hoverTimeout;
 
   const handleMouseEnter = (anime) => {
-    hoverTimeout = setTimeout(() => {
-      setSelectedAnime(anime);
-      setShowModal(true);
-    },1500);
+    setSelectedAnime(anime);
+    setShowModal(true);
   };
 
   const handleMouseLeave = () => {
-    clearTimeout(hoverTimeout);
     setShowModal(false);
   };
 
@@ -55,7 +51,6 @@ export default function Anime() {
     }
   };
 
-
   return (
     <>
       <div className="flex justify-center mt-8">
@@ -75,73 +70,88 @@ export default function Anime() {
           Next Page
         </button>
       </div>
-        {/* Anime cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+      {/* Anime cards */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-3">
         {animeData.map((anime) => (
           <div
             key={anime.title}
             className="bg-white p-4 rounded shadow-md flex h-50 w-70"
-            onMouseEnter={() => handleMouseEnter(anime)}
-            onMouseLeave={handleMouseLeave}
+            onClick={() => handleMouseEnter(anime)}
           >
-            <div className="flex items-center justify-center w-5 h-10 rounded-full mr-3">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full mr-3">
               <p>Rank:</p>
               <p className="text-red-500 font-semibold"> {anime.ranking}</p>
             </div>
             <img
               src={anime.image}
               alt={anime.title}
-              className="w-40 h-48  object-cover mb-4 rounded"
+              className="w-40 h-48  object-cover mb-4 rounded gap-2"
             />
-             
-             <div className="flex flex-col">
-        <h2 className="text-xl font-semibold mb-2">{anime.title}</h2>
-        <p className="text-gray-600 text-sm mb-1">Status: {anime.status}</p>
-        <p className="text-gray-600 text-sm mb-1">Episodes: {anime.episodes}</p>
-        <div className="flex flex-wrap mt-2">
-          {anime.genres.map((genre) => (
-            <span
-              key={genre}
-              className="inline-block bg-gray-200 rounded-full px-2 py-1 mt-2 mr-2 text-sm text-gray-700"
-            >
-              {genre}
-            </span>
-          ))}
-        </div>
-      </div>
-    </div>
+
+            <div className="flex flex-col left-2">
+              <h2 className="text-xl font-semibold mb-2">{anime.title}</h2>
+              <p className="text-gray-600 text-sm mb-1">
+                Status: {anime.status}
+              </p>
+              <p className="text-gray-600 text-sm mb-1">
+                Episodes: {anime.episodes}
+              </p>
+              <div className="flex flex-wrap mt-2">
+                {anime.genres.map((genre) => (
+                  <span
+                    key={genre}
+                    className="inline-block bg-gray-200 rounded-full px-2 py-1 mt-2 mr-2 text-sm text-gray-700"
+                  >
+                    {genre}
+                  </span>
+                ))}
+              </div>
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold h-8 w-9"
+                onClick={() => handleMouseEnter(anime)}
+              >
+                Read more ...
+              </button>
+            </div>
+          </div>
         ))}
       </div>
-      
 
       {/* Modal */}
       {showModal && selectedAnime && (
         <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50">
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded ml-4"
+            onClick={handleMouseLeave}
+          >
+            close
+          </button>
           <div className="bg-white p-4 rounded shadow-md">
-            <h2 className="text-xl font-semibold mb-2">{selectedAnime.title}</h2>
+            <h2 className="text-xl font-semibold mb-2">
+              {selectedAnime.title}
+            </h2>
             <p className="text-black">{selectedAnime.synopsis}</p>
           </div>
         </div>
       )}
 
-        <div className="flex justify-center mt-8">
-          {pageNumber > 1 && (
-            <button
-              onClick={handlePageDecrease}
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mr-4"
-            >
-              Previous Page
-            </button>
-          )}
-          <p className="text-gray-700">Page: {pageNumber}</p>
+      <div className="flex justify-center mt-8">
+        {pageNumber > 1 && (
           <button
-            onClick={handlePageIncrease}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded ml-4"
+            onClick={handlePageDecrease}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mr-4"
           >
-            Next Page
+            Previous Page
           </button>
-        </div>
-      
+        )}
+        <p className="text-gray-700">Page: {pageNumber}</p>
+        <button
+          onClick={handlePageIncrease}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded ml-4"
+        >
+          Next Page
+        </button>
+      </div>
     </>
   );
 }
